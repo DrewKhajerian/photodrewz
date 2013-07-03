b9025493ce478e5c151356dd0bdf7cd3fec76a31
DrewKhajerian <arkhajer@gmail.com>
2013-07-03 15:39:33 -0400

    refactored routes with resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,17 @@ Photodrewz::Application.routes.draw do
  
   devise_for :users
 
-  post 'pictures' => 'pictures#create'
+  # dont need these once resources is used
+  # post 'pictures' => 'pictures#create'
 
-  get 'pictures' => 'pictures#index'
+  # get 'pictures' => 'pictures#index'
+  # get 'pictures/new' => 'pictures#new'
+  # get 'pictures/:id' => 'pictures#show', as: "picture"
+  # get 'pictures/:id/edit' => 'pictures#edit' as: 'edit_picture'
 
-  get 'pictures/new' => 'pictures#new'
-  get 'pictures/:id' => 'pictures#show', as: "picture"
-
-  get 'pictures/:id/edit' => 'pictures#edit' as: 'edit_picture'
+  # resources allows for create, read, edit, delete in one line
+  # Follows CRUD
+  resources :pictures 
 
   root :to => 'pictures#index'
 
