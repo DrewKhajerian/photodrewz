b26a4692062beb57acfebda4992bfa36e915e17c
DrewKhajerian <arkhajer@gmail.com>
2013-07-03 14:30:30 -0400

    save new photos to db

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,7 +3,7 @@ class PicturesController < ApplicationController
 	def index
 		@pictures = Picture.all
 	end
-	
+
   def show
     @picture = Picture.find(params[:id])
   end
@@ -12,8 +12,17 @@ class PicturesController < ApplicationController
   end
 
   def create
-  	render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
-  end
+  	# creates a new picture instance through the new form
+  	@picture = Picture.new(params[:picture])
+		
+		if @picture.save
+	    # if the save for the picture was successful, go to index.html.erb
+	    redirect_to pictures_url
+  	else
+	    # otherwise render the view associated with the action :new (i.e. new.html.erb)
+	    render :new
+  	end
 
+  end
 
 end
