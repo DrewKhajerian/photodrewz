26bf31be10f08a7de059ceb8517df05ede5a70e8
DrewKhajerian <arkhajer@gmail.com>
2013-07-03 12:25:31 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photodrewz::Application.config.secret_token = '2f1e25d34db76027b533a8a6e1c24464dc452a6fb6bca6d2c42f76320331b84c8165616116738ce4f14204ec1121f0bc7969518da3c58f6596b21754900e7cf2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Photodrewz::Application.config.session_store :cookie_store, key: '_photodrewz_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Photodrewz::Application.config.session_store :active_record_store
